9578a7f1391edd53871b6739a81d7dbc54554cd2
MenachoMarian <marian.menacho369@gmail.com>
2019-07-08 17:02:47 -0400

    MOdificando esquemq producto

--- database/product.js
+++ database/product.js
@@ -1,11 +1,14 @@
 const mongoose = require("./connect");
 var PRODUCTSCHEMA = {
+  //idusuario:   String,
   nombre:      String,
   precio:      Number,
   stock:       Number,
   categoria:   String,
   descripcion: String,
   register:    Date,
+  emailuser:   String,
+  estado:      String,
 }
 
 const PRODUCT = mongoose.model("product", PRODUCTSCHEMA);
